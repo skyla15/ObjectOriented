e1c6b2a8a3e2105e20e2ff19c6a74603322f100e
Mac <>
2018-06-02 22:51:38 +0900

    공학 / 프로그래머용 기능 추가 / 로직 부분 수정중

--- CalcProject/src/gui/Calculator.java
+++ CalcProject/src/gui/Calculator.java
@@ -18,15 +18,20 @@ public class Calculator
 	private static int calcState = 0; //0:basic; 1:engineering
 	
 	private JFrame frame = new JFrame("Calculator");
+	// 추가 
+	private JPanel normalCalculator3 = new JPanel(); 
 	private JPanel normalCalculator2 = new JPanel();
 	private JPanel normalCalculator = new JPanel();
 	
 	private JPanel northPanel = new JPanel();
 	private JPanel bottomPanel = new JPanel();
+	
 	private JPanel cardPanel = new JPanel();
 	private CardLayout card = new CardLayout();
+	
 	private JPanel centerBasicPanel = new JPanel();
 	private JPanel centerEngineeringPanel = new JPanel();
+	private JPanel centerProgrammerPanel = new JPanel(); // 프로그래머용 
 	private JPanel eastPanel = new JPanel();
 	
 	private String code = "";
@@ -36,29 +41,50 @@ public class Calculator
 	private JLabel exprStackLabel[] = new JLabel[5];
 	private JLabel expressionLabel;
 	private JTextField resultWindow;
-	private JLabel copyRightLabel = new JLabel("20XX11XXXX - who\'s");
+	private JLabel copyRightLabel = new JLabel("2013112130 - Jaeyeop's");
 	
 	final String[] opGeneralSet = {"save", "load"};	
 	final String[] opSet = {"C", "<", "%", "+", "-", "*", "/", "^", ".", "="};
 	final String[] opEngSet = {"log", "exp", "sin", "cos", "tan"};
+	
+	//프로그래머용 계산기
+	final String[] opLogicSet = {"and", "xor", "or", "not"};
+	final String[] opBinSet = { "Uno", "Dos", "Bin", "Hex", "Oct"};
+	private boolean checkLogic;
+	private boolean checkBin;
+	
 	private JButton opGeneralButtons[] = new JButton[opGeneralSet.length];
 	private JButton opButtons[] = new JButton[opSet.length];
 	private JButton opButtons2[] = new JButton[opSet.length];
+	private JButton opButtons3[] = new JButton[opSet.length];
+	
+	//공학용 계산기 
 	private JButton opEngButtons[] = new JButton[opEngSet.length];
-	private JButton numberButtons[] = new JButton[10];
-	private JButton numberButtons2[] = new JButton[10];
+	
+	//프로그래머용 계산기 
+	private JButton opLogicButtons[] = new JButton[opLogicSet.length];
+	private JButton opBinButtons[] = new JButton[opBinSet.length];
+	
+	//각 용도마다 숫자 버튼 설정 
+	private JButton numberButtons[] = new JButton[10]; // 일반 
+	private JButton numberButtons2[] = new JButton[10]; // 공학 
+	private JButton numberButtons3[] = new JButton[10]; // 프로그래머 
 
+	//Calculator Listener 
 	class CalcListener implements ActionListener {
 		ArrayList<String> postfix;
 		ArrayList<Integer> state;
 		Stack<String> opStack;
+		
 		boolean isDecimal(char x) {
 			return '0' <= x && x <= '9';
 		}
+		
 		boolean isUnaryOp(char x) {
-			if(x == '<' || x == 'l' || x == 's' || x == 'c' || x == 't') return true;
+			if(x == '<' || x == 'l' || x == 's' || x == 'c' || x == 't' || x == 'B' || x == 'O' || x == 'H' || x =='U' || x == 'D') return true;
 			return false;
 		}
+		
 		private String solve(String str) {
 			postfix = new ArrayList<String>();
 			state = new ArrayList<Integer>();
@@ -69,16 +95,18 @@ public class Calculator
 
 				char s = str.charAt(to);
 				if(isDecimal(s)) {
-					//숫자 인식
+					// 입력된 식의 숫자 인식
 					while(isDecimal(s) || s == '.') { 
 						to++;
 						if(to >= str.length()) break;
 						s = str.charAt(to); 
 					}
 					i = to-1;
+					
 					postfix.add(str.substring(from, to));
 					state.add(0);
 				}
+				
 				else {
 					//연산자 인식
 					char op = str.charAt(i);
@@ -93,6 +121,7 @@ public class Calculator
 						}
 						opStack.push("" + op);
 					}
+					
 					else { 
 						//우선순위 높음
 						while(!opStack.empty()) {
@@ -107,6 +136,7 @@ public class Calculator
 					}
 				}
 			}
+			
 			while(!opStack.empty()) {
 				String t = opStack.pop();
 				postfix.add(t);
@@ -114,27 +144,75 @@ public class Calculator
 			}
 			
 			Stack<Double> res = new Stack<Double>();
+			
 			for(int i=0;i<postfix.size();i++) {
 				String s = postfix.get(i);
-				if(state.get(i) == 0)
+				if(state.get(i) == 0) // state가 0 일경우 숫, 입력된 숫자를 더블형으로 변환하여 저장 
 					res.push(Double.parseDouble(s));
 				else {
 					char op = s.charAt(0);
 					double r1 = 0, r2 = 0;
+					
 					r1 = res.pop();
+					
 					if(op == '<') // unary
 						res.push(Math.sqrt(r1));
-					else if(op == 'l')
+					else if(op == 'l') // log 
 						res.push(Math.log10(r1));
-					else if(op == 's')
+					else if(op == 's') // sin 
 						res.push(Math.sin(r1));
-					else if(op == 'c')
+					else if(op == 'c') // cos 
 						res.push(Math.cos(r1));
-					else if(op == 't')
+					else if(op == 't') // tan 
 						res.push(Math.tan(r1));
+					
 
-					else { //binary
+					// Binary 연산 추가 
+					else if(op == 'B') {//to Binar
+						resultWindow.setText(convertTo_Bin(r1));
+						res.push(Double.parseDouble(convertTo_Bin(r1)));
+						checkBin = true;
+
+						return null;
+					}
+					else if(op == 'H') { //to Hex
+						resultWindow.setText(convertTo_Hex(r1));
+						res.push(Double.parseDouble(convertTo_Hex(r1)));	
+
+						checkBin = true;
+						return null;
+					}
+					else if(op == 'O') { //to Octal
+						resultWindow.setText(convertTo_Oct(r1));
+						res.push(Double.parseDouble(convertTo_Oct(r1)));
+
+						checkBin = true;
+						return null;
+					}
+					else if(op == 'U') { //to 1's complement
+						resultWindow.setText(convertTo_1s_Complement(r1));
+						res.push(Double.parseDouble(convertTo_1s_Complement(r1)));
+
+						checkBin = true;
+						return null;
+					}
+					else if(op == 'D') {//to 2's complement
+						resultWindow.setText(convertTo_2s_Complement(r1));
+						res.push(Double.parseDouble(convertTo_2s_Complement(r1)));	
+
+						checkBin = true;
+						return null;
+					}
+					else if(op == 'n') { //not
+
+						checkLogic = true;
+						return null;
+					}
+						
+					else { //binary, 이항연산 
+						
 						r2 = res.pop();
+						
 						if(op == '+')
 							res.push(r1+r2);
 						if(op == '-')
@@ -147,12 +225,91 @@ public class Calculator
 							res.push(Math.pow(r2, r1));
 						if(op == 'e')
 							res.push(r2 * Math.pow(10, r1));
+						// Logic 추가 
+						if(op == 'a') {
+	
+							checkLogic = true;//and
+							return null;
+						}
+						if(op == 'x') { //xor
+
+							checkLogic = true;
+							return null;
+						}
+						if(op == 'o') { //or
+							checkLogic = true;
+							return null;
+						}
+
 					}
 				}
 			}
 			
 			return String.valueOf(res.peek());
 		}
+		
+		
+		//2진 변환 
+		public String convertTo_Bin(double input) {
+			int x = (int)input;
+			String res = "";
+			ArrayList binary = new ArrayList();
+			while(x != 0) {
+				binary.add(x%2);
+				x = x/2;
+			}			
+			for(int i = binary.size() ; i > 0 ; i--) {
+				res += binary.get(i-1);
+			}
+			return res;
+		}
+
+		//8진 변환 
+		public String convertTo_Oct(double input) {
+			int x = (int)input;
+			String res = "";
+			ArrayList octal = new ArrayList();
+			while(x != 0) {
+				octal.add(x%8);
+				x = x/8;
+			}
+			
+			for(int i = octal.size() ; i > 0 ; i--) {
+				res += octal.get(i-1);
+			}
+			return res;
+		}
+		
+		//16진수 변환 
+		public String convertTo_Hex(double input) {
+			int x = (int)input;
+			String res = "";
+			res = String.format("%x",  x);
+			return res;
+		}
+		
+		//1의 보수 변환
+		public String convertTo_1s_Complement(double input) {
+			int x = (int)input;
+			String res ="";
+			res += Integer.toBinaryString(~x);
+			return res;
+		}
+		
+		//2의 보수 변환 
+		public String convertTo_2s_Complement(double input) {
+			int x = (int)input;
+			String res = "";
+			x = ~x + 1;
+			res += Integer.toBinaryString(x);
+			System.out.println(res);
+			return res;
+		}
+		
+		
+		
+		// 로직 연산 추가 
+		
 		void refreshMemoryStack() {
 			for(int i=0;i<exprStack.size();i++) {
 				String t = exprStack.get(i);
@@ -161,11 +318,14 @@ public class Calculator
 			for(int i=exprStack.size(); i<5;i++)
 				exprStackLabel[i].setText("");
 		}
+		
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			JButton b = (JButton)e.getSource();
 			String name = b.getText();
+			
+
 			if(name.equals("save")) {
 				if(codeStack.size() < 5) {
 					codeStack.push(code);
@@ -173,6 +333,7 @@ public class Calculator
 					refreshMemoryStack();
 				}
 			}
+			
 			else if(name.equals("load")) {
 				if(!codeStack.empty()) {
 					code = codeStack.pop();
@@ -181,13 +342,26 @@ public class Calculator
 					refreshMemoryStack();
 				}
 			}
+			
 			else if(name.equals("C")) {
 				code = "";
 				expressionLabel.setText(" ");
+				resultWindow.setText(" ");
 			}
+			
 			else if(name.equals("=")) {
 				resultWindow.setText(solve(code));
 			}
+			
+			else if(checkLogic == true || checkBin == true) { // 프로그래머용 연산시 필요 
+				expressionLabel.setText(" ");
+				resultWindow.setText(" ");
+				code = "";
+				checkLogic = false;
+				checkBin = false;
+			}
+			
+			
 			else if(name.equals("%")) {
 				int i = 0;
 				for(i=code.length()-1;i>=0;i--) {
@@ -209,12 +383,14 @@ public class Calculator
 					
 				resultWindow.setText(res + "");				
 			}
+			
 			else {
-				for(int i=0;i<=9;i++)
+				for(int i=0;i<=9;i++) {					
 					if(b.getText().equals(""+i)) {
 						expressionLabel.setText(expressionLabel.getText()+i);
 						code += "" + i;
 					}
+				}
 				for(int i=0;i<opButtons.length;i++)
 					if(b.getText().equals(""+opSet[i])) {
 						if(opSet[i].equals("<"))
@@ -224,6 +400,7 @@ public class Calculator
 						code += opSet[i];
 						//root: r
 					}
+				
 				for(int i=0;i<opEngButtons.length;i++)
 					if(b.getText().equals(""+opEngSet[i])) {
 						if(i == 1)
@@ -233,6 +410,50 @@ public class Calculator
 						code += opEngSet[i].charAt(0); 
 						//log:l; exp:e; sin:s; cos:c; tan:t;
 					}
+				
+				// 작업할 곳 
+				//logic button
+				for(int i = 0 ; i< opLogicButtons.length ; i++)					
+					if(b.getText().equals(""+opLogicSet[i])) {
+						char op = b.getText().charAt(i);
+						int solval = Integer.parseInt(solve(code.substring(0,i)));
+						int perval = Integer.parseInt(solve(code.substring(i+1,code.length())));
+						int res = 0;
+						if(op == 'a')
+							res = solval & perval;
+						else if(op == 'x')
+							res = solval ^ perval;
+						else if(op == 'o')
+							res = solval | perval;
+						else if(op == 'n')
+							res = ~solval;
+							
+						resultWindow.setText(Integer.toBinaryString(res) + "");		
+					}
+				
+				//
+				
+				for(int i = 0 ; i < opBinSet.length ; i++) {
+					if(b.getText().equals(""+opBinSet[i])) {
+						if(b.getText().equals("Uno")) {
+							expressionLabel.setText("convert to 1's complement :" + expressionLabel.getText());
+							code += opBinSet[i].charAt(0);
+							solve(code);
+						}
+						else if(b.getText().equals("Dos")) {
+							expressionLabel.setText("convert to 2's complement :" + expressionLabel.getText());
+							code += opBinSet[i].charAt(0);
+							solve(code);
+						}
+						else{
+							expressionLabel.setText(opBinSet[i] + expressionLabel.getText());
+							code += opBinSet[i].charAt(0);
+							solve(code);
+						}
+
+					}
+				}
+				
 			}
 		}	
 	}
@@ -244,6 +465,7 @@ public class Calculator
 		gbc.gridheight = h;
 		p.add(c, gbc);
 	}
+	
 	@SuppressWarnings("unused")
 	public Calculator()
 	{
@@ -254,6 +476,8 @@ public class Calculator
 		{
 			normalCalculator.setLayout(new BorderLayout());
 			normalCalculator2.setLayout(new BorderLayout());
+			// 프로그래머용 추가 
+			normalCalculator3.setLayout(new BorderLayout());
 			
 			
 			NORTH:
@@ -262,33 +486,64 @@ public class Calculator
 				GridBagConstraints layoutRule = new GridBagConstraints();
 				JButton basicButton = new JButton("basic");
 				JButton engineeringButton = new JButton("engineering");
-				JButton programmerButton = new JButton("---------");
+				JButton programmerButton = new JButton("programmer"); // 프로그래머용 
 				JButton matrixButton = new JButton("-------");
+				
+				// Basic Calculator Show
 				ActionListener basicL = new ActionListener() {
-
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						// TODO Auto-generated method stub
 						card.show(cardPanel, "basic");
+						resultWindow.setText("");
+						expressionLabel.setText("");
+						opGeneralButtons[0].setEnabled(true);
+						opGeneralButtons[1].setEnabled(true);
+
 						calcState = 0;
 					}
 				};
+				
+				// Engineering Calculator Show
 				ActionListener engineeringL = new ActionListener() {
-
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						// TODO Auto-generated method stub
 						card.show(cardPanel, "engineering");
+						resultWindow.setText("");
+						expressionLabel.setText("");
 						calcState = 1;
+						opGeneralButtons[0].setEnabled(true);
+						opGeneralButtons[1].setEnabled(true);
 					}
 				};
+				
+				// dProgrammer Calculator Show
+				ActionListener programmerL = new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						card.show(cardPanel, "programmer");
+						resultWindow.setText("");
+						expressionLabel.setText("");
+						opGeneralButtons[0].setEnabled(false);
+						opGeneralButtons[1].setEnabled(false);
+						
+						calcState = 2;
+					}
+				};
+				
+				
 				basicButton.addActionListener(basicL);
 				engineeringButton.addActionListener(engineeringL);
-
+				programmerButton.addActionListener(programmerL);// 프로그래머용 
+				
 				layoutRule.fill = GridBagConstraints.BOTH;
 				layoutRule.weightx = 1.0;
 				layoutRule.weighty = 1.0;
 
+				// button added to panel 
 				addToPanel(northPanel, layoutRule, basicButton, 0, 1, 1, 1);
 				addToPanel(northPanel, layoutRule, engineeringButton, 1, 1, 1, 1);
 				addToPanel(northPanel, layoutRule, programmerButton, 2, 1, 1, 1);
@@ -298,7 +553,8 @@ public class Calculator
 				layoutRule.gridheight = 1;
 				layoutRule.fill = GridBagConstraints.HORIZONTAL;
 				
-				JLabel stringWindow = new JLabel("Title");
+				JLabel stringWindow = new JLabel("");
+				
 				expressionLabel = new JLabel(" ");
 				resultWindow = new JTextField(20);
 				
@@ -313,6 +569,7 @@ public class Calculator
 				northPanel.add(resultWindow, layoutRule);
 				
 			}
+			
 			EAST:
 			{
 				eastPanel.setLayout(new GridBagLayout());
@@ -320,12 +577,15 @@ public class Calculator
 				layoutRule.fill = GridBagConstraints.HORIZONTAL;
 				layoutRule.weightx = 1.0;
 				layoutRule.weighty = 1.0;
+				
 				exprStackRepLabel = new JLabel("---Memory Stack---");
 				addToPanel(eastPanel, layoutRule, exprStackRepLabel, 0, 0, 3, 1);
+				
 				for(int i=0;i<5;i++) {
 					exprStackLabel[i] = new JLabel("");
 					addToPanel(eastPanel, layoutRule, exprStackLabel[i], 0, i+1, 3, 1);
 				}
+				
 				for(int i=0;i<opGeneralButtons.length;i++) {
 					opGeneralButtons[i] = new JButton(opGeneralSet[i]);
 					addToPanel(eastPanel, layoutRule, opGeneralButtons[i], i, 6, 1, 1);
@@ -348,6 +608,8 @@ public class Calculator
 				
 				centerBasicPanel.setLayout(new GridBagLayout());
 				centerEngineeringPanel.setLayout(new GridBagLayout());
+				centerProgrammerPanel.setLayout(new GridBagLayout());
+				
 				GridBagConstraints layoutRule = new GridBagConstraints();
 				
 				layoutRule.gridwidth = 1;
@@ -357,25 +619,47 @@ public class Calculator
 				
 				layoutRule.fill = GridBagConstraints.CENTER;
 
+				// 일반 연산 버튼 추가 
 				for (int i =  0; i < opButtons.length; i++) {
 					opButtons[i] = new JButton(opSet[i]);
 					opButtons2[i] = new JButton(opSet[i]);
+					opButtons3[i] = new JButton(opSet[i]);
 					CalcListener c = new CalcListener();
 					opButtons[i].addActionListener(c);
 					opButtons2[i].addActionListener(c);
+					opButtons3[i].addActionListener(c);
 				}
+				
+				// 공학용 연산버튼 추가 
 				for(int i=0; i<opEngButtons.length; i++) {
 					opEngButtons[i] = new JButton(opEngSet[i]);
 					CalcListener c = new CalcListener();
 					opEngButtons[i].addActionListener(c);
 				}
 				
+				// 숫자를 추가 
 				for (int i = 0; i < 10; i++) {
 					numberButtons[i] = new JButton(String.valueOf(i));
 					numberButtons2[i] = new JButton(String.valueOf(i));
+					numberButtons3[i] = new JButton(String.valueOf(i));
 					CalcListener c = new CalcListener();
 					numberButtons[i].addActionListener(c);
 					numberButtons2[i].addActionListener(c);
+					numberButtons3[i].addActionListener(c);
+				}
+				
+				// 로직 계산용 버튼 추가 
+				for(int i = 0 ; i < opLogicButtons.length ; i++) {
+					opLogicButtons[i] = new JButton(opLogicSet[i]);
+					CalcListener c = new CalcListener();
+					opLogicButtons[i].addActionListener(c);
+				}
+				
+				for(int i = 0 ; i < opBinButtons.length ; i++) {
+					opBinButtons[i] = new JButton(opBinSet[i]);
+					CalcListener c = new CalcListener();
+					opBinButtons[i].addActionListener(c);
+				
 				}
 				
 				for (int i = 0; i < buttonHeight - 1; i++)
@@ -391,6 +675,7 @@ public class Calculator
 							layoutRule.gridx = j;
 							centerBasicPanel.add(numberButtons[numbering * 3 - 2 + j], layoutRule);
 							centerEngineeringPanel.add(numberButtons2[numbering * 3 - 2 + j], layoutRule);
+							centerProgrammerPanel.add(numberButtons3[numbering * 3 - 2 + j], layoutRule);
 						}
 						
 						for (int j = 3; j < buttonWidth; j++)
@@ -398,6 +683,7 @@ public class Calculator
 							layoutRule.gridx = j;
 							centerBasicPanel.add(opButtons[j + (i * (buttonWidth - 3))], layoutRule);
 							centerEngineeringPanel.add(opButtons2[j + (i * (buttonWidth - 3))], layoutRule);
+							centerProgrammerPanel.add(opButtons3[j + (i * (buttonWidth - 3))], layoutRule);
 						}
 					}
 					
@@ -408,6 +694,7 @@ public class Calculator
 							layoutRule.gridx = j;
 							centerBasicPanel.add(opButtons[j + (i * buttonWidth)], layoutRule);
 							centerEngineeringPanel.add(opButtons2[j + (i * buttonWidth)], layoutRule);
+							centerProgrammerPanel.add(opButtons3[j + (i * buttonWidth)], layoutRule);
 						}
 					}
 				}
@@ -417,26 +704,43 @@ public class Calculator
 				layoutRule.gridx = 0;
 				centerBasicPanel.add(opButtons[opButtons.length - buttonWidth + 1], layoutRule);
 				centerEngineeringPanel.add(opButtons2[opButtons2.length - buttonWidth + 1], layoutRule);
+				centerProgrammerPanel.add(opButtons3[opButtons2.length - buttonWidth + 1], layoutRule);
+
 				
 				layoutRule.gridx = 1;
 				centerBasicPanel.add(numberButtons[0], layoutRule);
 				centerEngineeringPanel.add(numberButtons2[0], layoutRule);
+				centerProgrammerPanel.add(numberButtons3[0], layoutRule);
+
 				
 				for (int i = opButtons.length - buttonWidth + 2; i < opButtons.length; i++)
 				{
 					layoutRule.gridx = i - opButtons.length + buttonWidth;
 					centerBasicPanel.add(opButtons[i], layoutRule);
 					centerEngineeringPanel.add(opButtons2[i], layoutRule);
+					centerProgrammerPanel.add(opButtons3[i], layoutRule);
 				}
 
+				//eng 버튼 추가 
 				for(int i=0;i<opEngButtons.length;i++) {
 					addToPanel(centerEngineeringPanel, layoutRule, opEngButtons[i], i, 6, 1, 1);
+				}
+				
+				//로직버튼 추가 
+				for(int i = 0 ; i < opLogicButtons.length ; i++) {
+					addToPanel(centerProgrammerPanel, layoutRule, opLogicButtons[i], i, 6, 1, 1);
+				}
+				
+				// 바이너리 변환 버튼 추가 
+				for(int i = 0 ; i < opBinButtons.length ; i++) {
+					addToPanel(centerProgrammerPanel, layoutRule, opBinButtons[i], 5, i, 1, 1);
 				}
 				
 
 				cardPanel.setLayout(card);
 				cardPanel.add("basic", centerBasicPanel);
 				cardPanel.add("engineering", centerEngineeringPanel);
+				cardPanel.add("programmer", centerProgrammerPanel);
 				card.first(cardPanel);
 			}
 		
